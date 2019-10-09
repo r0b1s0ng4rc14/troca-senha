2b1af32f82e5f5b51ed02d1027cc2da8e5d7671d
Robison Geraldi Garcia <s2it_rgarcia@uolinc.com>
2019-10-09 16:38:31 -0300

    Ajustando o código

--- copy_script.sh
+++ copy_script.sh
@@ -13,12 +13,14 @@ usuario="ubuntu"
 
 for ssh in "${server[@]}"
         do 
-                sshpass -p $senha scp -P 2222 -o StrictHostKeyChecking=no troca-senha.sh  $usuario@$ssh:.
                 echo -e "$GREEN Copiando arquivos...\n $RED" $ssh $NORMAL
-                
-                sshpass -p $senha ssh -p 2222 ubuntu@$ssh "printf '$senha' | sudo -S bash troca-senha.sh"
+                sshpass -p $senha scp -P 2222 -o StrictHostKeyChecking=no troca-senha.sh  $usuario@$ssh:.
+                echo -e "\n"
                 echo -e "$GREEN Executando scripts...\n $RED" $ssh $NORMAL
-            
-                sshpass -p $senha ssh -p 2222 ubuntu@$ssh "printf '$senha' | sudo -S rm -f troca-senha.sh"    
-                echo -e "$GREEN Limpando arquivos temporários...\n $RED $ssh \n" $NORMAL
+                sshpass -p $senha ssh -p 2222 ubuntu@$ssh "printf '$senha' | sudo -S bash troca-senha.sh"
+                echo -e "\n"
+                echo -e "$GREEN Limpando arquivos temporários...\n $RED $ssh \n" $NORMAL            
+                sshpass -p $senha ssh -p 2222 ubuntu@$ssh "printf '$senha' | sudo -S rm -f troca-senha.sh"
+                echo -e "\n"    
+                echo -e "$BLUE FIM \n"
 done
--- troca-senha.sh
+++ troca-senha.sh
@@ -4,7 +4,7 @@
 #"user" "uid" "gid" "senha_em_hash"
 
 login=("user1" "user2" "user3" "user4" "user5")
-uid=("0001" "0002" "0003" "0004" "0005")
+uid=("9001" "9002" "9003" "9004" "9005")
 guid="6000"
 senha=("\$6\$J.8DdsPg8Q\$4vkvaI4GdmLJujaC02GRXdFrzN0lxv1j/gspYlcNkHXPRE/aj0zbj6T8rdCdHYzS5hg3a9DtC4W/BBAvH6QCI1" "\$6\$J.8DdsPg8Q\$4vkvaI4GdmLJujaC02GRXdFrzN0lxv1j/gspYlcNkHXPRE/aj0zbj6T8rdCdHYzS5hg3a9DtC4W/BBAvH6QCI1" "\$6\$J.8DdsPg8Q\$4vkvaI4GdmLJujaC02GRXdFrzN0lxv1j/gspYlcNkHXPRE/aj0zbj6T8rdCdHYzS5hg3a9DtC4W/BBAvH6QCI1" "\$6\$J.8DdsPg8Q\$4vkvaI4GdmLJujaC02GRXdFrzN0lxv1j/gspYlcNkHXPRE/aj0zbj6T8rdCdHYzS5hg3a9DtC4W/BBAvH6QCI1" "\$6\$J.8DdsPg8Q\$4vkvaI4GdmLJujaC02GRXdFrzN0lxv1j/gspYlcNkHXPRE/aj0zbj6T8rdCdHYzS5hg3a9DtC4W/BBAvH6QCI1")
 
@@ -17,7 +17,7 @@ for (( l=0; l<=3; l++ ))
                     else
                         echo "usuario nao existe"
                         echo ${login[$l]} ${uid[$l]} $guid ${senha[$l]}
-                        echo -e "useradd -m -s /bin/bash -u $id_user -g $id_group -p $pass $user"
+                        useradd -m -s /bin/bash -u ${uid[$l]} -g $guid -p ${senha[$l]} ${login[$l]}
                     fi
         
 done
