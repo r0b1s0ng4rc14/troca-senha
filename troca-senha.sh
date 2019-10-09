64a0555e0483f787b585ea1ac08efa175407a50d
Robison Geraldi Garcia <s2it_rgarcia@uolinc.com>
2019-10-09 16:46:48 -0300

    Ajustando o cores de saida

--- troca-senha.sh
+++ troca-senha.sh
@@ -1,8 +1,13 @@
 #!bin/bash
 
+
+RED="\033[31m"
+GREEN="\033[32m"
+NORMAL="\033[0;39m"
+BLUE="\e[34m"
+
 #Hash todo caracter especial deve ser adicionado "\"
 #"user" "uid" "gid" "senha_em_hash"
-
 login=("user1" "user2" "user3" "user4" "user5")
 uid=("9001" "9002" "9003" "9004" "9005")
 guid="6000"
@@ -11,13 +16,11 @@ senha=("\$6\$J.8DdsPg8Q\$4vkvaI4GdmLJujaC02GRXdFrzN0lxv1j/gspYlcNkHXPRE/aj0zbj6T
 for (( l=0; l<=3; l++ ))
     do 
         if sudo grep -q ${login[$l]} /etc/shadow; then
-                        echo "usuario existe"
                         usermod -p ${login[$l]} ${login[$l]}
-                        echo ${login[$l]} ${uid[$l]} $guid ${senha[$l]}
+                        echo -e "$BLUE #-----Usuário: $RED ${login[$l]} $BLUE existe, trocando a senha-----# $NORMAL"
                     else
-                        echo "usuario nao existe"
-                        echo ${login[$l]} ${uid[$l]} $guid ${senha[$l]}
                         useradd -m -s /bin/bash -u ${uid[$l]} -g $guid -p ${senha[$l]} ${login[$l]}
+                        echo -e "$GREEN #-----Usuário: $RED ${login[$l]} $GREEN não existe, criando-----# $NORMAL"
                     fi
         
 done
